bdb16efa4a44074564cbb20162ee96c6136e2adc
designlabmo <59714838+milenaor@users.noreply.github.com>
2021-05-03 12:10:21 -0500

    V_4

--- Barras_DF_Viz/sketch.js
+++ Barras_DF_Viz/sketch.js
@@ -9,7 +9,9 @@ let mountains;
 
 let data=[4941, 4739, 2303, 2064, 1638, 1493, 1290, 1136, 973, 805, 791, 670, 454, 377, 303, 275, 273, 249, 235, 234, 186, 143, 128, 105, 94, 79, 41, 38, 23, 21, 2];
 
-let labels=[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, V, W, X, Y, Z, AA, BB, CC, DD, EE, FF];
+let namedata=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
+//let texto=[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, V, W, X, Y, Z, AA, BB, CC, DD, EE, FF];
 
 
 /*let labels =[Campesino, Trabajador_de_Finca, Trabajo_Sin_Especificar, Comerciante, Economia_Informal, Empleado, Conductor_Motorista, Otra_Cual, Obrero, Desempleado, Estudiante, Fuerza_Publica, Paramilitar, Pescador, Minero, Administrador_de_Finca, Raspachin, Funcionario_Publico, Seguridad_Privada, Profesional, Ganadero_Hacendado, No_Aplica, Delincuente, Guerrillero, Personal_de_Salud, Ama_de_Casa, Religioso, Trabajador_Sexual, Empresario_Industrial, Pensionado,Grupo_Posdesmovilizacion];*/
@@ -66,7 +68,7 @@ function setup(){
     grafico(data);
     
     //--Nombres de Gráfico de barras--//
-    ejex (labels);
+    ejex (namedata);
 
 }
 
@@ -99,7 +101,7 @@ function barra(y,largo){
 //-------función para crear el gráfico de barras--------//
 
 function grafico (data){
-    anchobanda= 1560/data.length;
+    anchobanda= ancho_c/data.length;
     
     for(let i=0; i < data.length; i++){
         let mapeo = map(data[i], 0, max(data), 0,alto_c)
@@ -109,11 +111,11 @@ function grafico (data){
 
 // -------función texto barras-------------//
 
-function ejex (labels){
-    fill(0);
+function ejex (namedata){
+    fill(23,78,99);
     
-    for (let i=0; i < labels.length; i++){
-        text(labels[i],0,i*anchobanda)
+    for (let i=0; i < namedata.length; i++){
+        text(namedata[i],(i*anchobanda)+12,725)
     }
     
 }
