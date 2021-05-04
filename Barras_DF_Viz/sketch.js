37200603b127673ac50819e828895f3f11970a21
designlabmo <59714838+milenaor@users.noreply.github.com>
2021-05-03 23:04:53 -0500

    V_7

--- Barras_DF_Viz/sketch.js
+++ Barras_DF_Viz/sketch.js
@@ -1,11 +1,10 @@
-//let labels =[Campesino, Trabajador_de_Finca, Trabajo_Sin_Especificar, Comerciante, Economia_Informal, Empleado, Conductor_Motorista, Otra_Cual, Obrero, Desempleado, Estudiante, Fuerza_Publica, Paramilitar, Pescador, Minero, Administrador_de_Finca, Raspachin, Funcionario_Publico, Seguridad_Privada, Profesional, Ganadero_Hacendado, No_Aplica, Delincuente, Guerrillero, Personal_de_Salud, Ama_de_Casa, Religioso, Trabajador_Sexual, Empresario_Industrial, Pensionado,Grupo_Posdesmovilizacion];//
-
 let data=[4941, 4739, 2303, 2064, 1638, 1493, 1290, 1136, 973, 805, 791, 670, 454, 377, 303, 275, 273, 249, 235, 234, 186, 143, 128, 105, 94, 79, 41, 38, 23, 21, 2];
 
 let MARGIN = 10;
-let FIRST_SECTION_HEIGHT = 500;
+let FIRST_SECTION_HEIGHT = 740;
 let CIRCLE_RADIUS_FACTOR = 80;
 let CIRCLE_SPACE = 5;
+let BACKGROUND_IMAGE_HEIGHT = 0.4
 
 function setup() {
     createCanvas(windowWidth-MARGIN, 2000);
@@ -26,23 +25,17 @@ function circleBar(x_bar, y_bar, height, space, circle_radius) {
 function draw() {
     fill(66,87,109);
     rect(0,0,width-10,FIRST_SECTION_HEIGHT);
-    image(img_background_mountains,0,0,width-MARGIN,FIRST_SECTION_HEIGHT);
+    image(img_background_mountains,0,FIRST_SECTION_HEIGHT*(1-BACKGROUND_IMAGE_HEIGHT),width-MARGIN,FIRST_SECTION_HEIGHT*BACKGROUND_IMAGE_HEIGHT);
     let bar_number = 30;
     for (let bar=0; bar<=bar_number; bar=bar+1){
-        bar_height = map(data[bar], 0, max(data), 0, FIRST_SECTION_HEIGHT - 50)
-        circle_radius = width/CIRCLE_RADIUS_FACTOR
-        circleBar(30+bar*(width-70)/bar_number,FIRST_SECTION_HEIGHT - 30,bar_height,CIRCLE_SPACE,circle_radius);
+        bar_height = map(data[bar], 0, max(data), 0, FIRST_SECTION_HEIGHT - 50);
+        circle_radius = width/CIRCLE_RADIUS_FACTOR;
+        fill (86,125,157);
+        rect(30+bar*(width-70)/bar_number-circle_radius*0.6, FIRST_SECTION_HEIGHT - bar_height - 25, circle_radius*1.2, bar_height+25);
+        circleBar(30+bar*(width-70)/bar_number,FIRST_SECTION_HEIGHT - 20,bar_height,CIRCLE_SPACE,circle_radius);
     }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
-
-
-
-
-
-
-
