ed4ef6c37b7f0b20025cd10443b0af00a90bac1e
designlabmo <59714838+milenaor@users.noreply.github.com>
2021-05-22 20:34:29 -0500

    etnico_Viz_2

--- enfoque_etnico_DF_Viz/sketch.js
+++ enfoque_etnico_DF_Viz/sketch.js
@@ -1,10 +1,10 @@
-//let dataEtnia=[79514, 661, 464,31, 3, 1];
-
+let dataEtnia=[79514, 661, 464,31, 3, 1];
+//79514, 661, 464,31, 3, 1
 //let namedataEtnia ["sin informacion","Indigena","Afrocolombiano","Raizal","Palenquero","Rom"];
 
 //Variables Canvas//
 let MARGIN = 10;
-let FIRST_SECTION_HEIGHT = 900;
+let FIRST_SECTION_HEIGHT = 5000;
 let BACKGROUND_IMAGE_HEIGHT = 0.4;
 
 
@@ -12,12 +12,58 @@ function setup() {
     createCanvas(windowWidth-MARGIN, FIRST_SECTION_HEIGHT);
     textFont("Fauna One"); 
     
-    for (let i=0; i < 50; i++){
-        let x= random (windowWidth-MARGIN);
-        let y= random (FIRST_SECTION_HEIGHT);
-        let r= 5;
+    //Sin información //
+    for (var i=0; i < dataEtnia[0]; i++){
+        var x= random (windowWidth-MARGIN-250);
+        var y= random (FIRST_SECTION_HEIGHT);
+        var r= 18;
         fill(216, 223, 229);
-        ellipse (x,y,r,r):
+        ellipse (x,y,r,r);
+    }
+    
+    //Indígena//
+     for (var i=0; i < dataEtnia[1]; i++){
+        var x= random (windowWidth-MARGIN-250);
+        var y= random (FIRST_SECTION_HEIGHT);
+        var r= 18;
+        fill(143, 169, 191);
+        ellipse (x,y,r,r);
+    }
+    
+    //Afrocolombiano//
+     for (var i=0; i < dataEtnia[2]; i++){
+        var x= random (windowWidth-MARGIN-250);
+        var y= random (FIRST_SECTION_HEIGHT);
+        var r= 18;
+        fill(7, 58, 86);
+        ellipse (x,y,r,r);
+    }
+    
+    //Raizal//
+     for (var i=0; i < dataEtnia[3]; i++){
+        var x= random (windowWidth-MARGIN-250);
+        var y= random (FIRST_SECTION_HEIGHT);
+        var r= 18;
+        fill(75, 94, 107);
+        ellipse (x,y,r,r);
+    }
+    
+    //Palenquero//
+    for (var i=0; i < dataEtnia[4]; i++){
+        var x= random (windowWidth-MARGIN-250);
+        var y= random (FIRST_SECTION_HEIGHT);
+        var r= 18;
+        fill(164, 179, 193);
+        ellipse (x,y,r,r);
+    }
+    
+    //Rom//
+     for (var i=0; i < dataEtnia[5]; i++){
+        var x= random (windowWidth-MARGIN-250);
+        var y= random (FIRST_SECTION_HEIGHT);
+        var r= 18;
+        fill(30, 42, 54);
+        ellipse (x,y,r,r);
     }
     
 }  
